5c779c15d8bbad23a0b52fc81275567089a43f34
Mukanahallipathna Satish Kedarnag <kedarnag_mukanahalli_from.tebs@stb.gov.sg>
2019-12-01 23:31:14 +0800

    [master] :sparkles: Service to pull all the packages and also to get information about particular package.

--- app/services/package_service.rb
+++ app/services/package_service.rb
@@ -1,5 +1,7 @@
 require "uri"
 require "net/http"
+require "dcf"
+require "open-uri"
 class PackageService
 
   def self.get_all_packages
@@ -9,10 +11,23 @@ class PackageService
     http.verify_mode = OpenSSL::SSL::VERIFY_PEER
     request = Net::HTTP::Get.new(uri.path)
     response = http.request(request)
-    response_body = JSON.parse(response.body)
-    
-    binding.pry
-    
+    packages = Dcf.parse(response.body)
+    self.build_url_to_get_package_information(packages)
+  end
+
+  private
+
+  def self.build_url_to_get_package_information(packages)
+    packages.each do |package|
+      open("#{Rails.root}/public/#{package["Package"]}_#{package["Version"]}.tar.gz", "wb", encoding: "ascii-8bit") do |file|
+        file << open("http://cran.r-project.org/src/contrib/#{package["Package"]}_#{package["Version"]}.tar.gz").read
+      end
+      Dir.chdir("public/") do
+        `tar xzf #{Rails.root}/public/#{package["Package"]}_#{package["Version"]}.tar.gz`
+      end
+      package_information = File.read("#{Rails.root}/public/#{package["Package"]}/DESCRIPTION")
+      response_body = Dcf.parse(package_information).first
+    end
   end
 
 end
