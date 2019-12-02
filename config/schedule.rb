c11ee2b42e6675568d661d1737df7af79bba064e
Mukanahallipathna Satish Kedarnag <kedarnag_mukanahalli_from.tebs@stb.gov.sg>
2019-12-02 11:41:54 +0800

    [master] :sparkles: Adds crob job to schedule task to pull packages.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 case @environment
 when "development"
-  every :day, at: '10:12am' do
+  every :day, at: '12:00pm' do
     puts "========START=============="
     runner "PackageService.get_all_packages", :output => "log/get_all_packages.log"
     puts "========FINISH=============="
